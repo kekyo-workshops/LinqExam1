e9e9e1e4897a3057fcd720662bffea95d0e071f2
Kouji Matsui <k@kekyo.net>
2016-09-27 14:56:58 +0900

    Initial commit.

---
+++ LinqExam1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqExam1
+{
+    class Program
+    {
+        #region Infinite
+        private static IEnumerable<int> Infinite(int value)
+        {
+            while (true)
+            {
+                yield return value;
+            }
+        }
+        #endregion
+
+        private static int[] CreateRandomData(int count)
+        {
+            return null;  // ...
+        }
+
+        private static void TestRandomData()
+        {
+            var data = CreateRandomData(100);
+            Console.WriteLine(string.Join(",", data));
+        }
+
+        static void Main(string[] args)
+        {
+            TestRandomData();
+        }
+    }
+}
